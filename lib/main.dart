532725364e078e7135ce586c4d49a82572072a00
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-12-28 03:20:02 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:crazy_slots/Screens/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crazy Slots',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomePage.id: (context) => HomePage(),
+      },
+      initialRoute: HomePage.id,
+    );
+  }
+}
