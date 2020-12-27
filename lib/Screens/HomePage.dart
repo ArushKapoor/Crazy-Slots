7d9a98b3d994e51f67a24eb3a867f80c3a316fe6
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-12-28 03:55:12 +0530

    added basic structural ui

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,8 +10,90 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    Container createSlot({String first, String second, String third}) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+        ),
+        padding: EdgeInsets.symmetric(
+            horizontal: _width * 0.02, vertical: _height * 0.01),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: _width * 0.02, vertical: _height * 0.015),
+              child: Text(
+                first,
+                style: TextStyle(fontSize: _height * 0.021),
+              ),
+            ),
+            SizedBox(
+              height: 1,
+              child: Container(
+                color: Colors.grey,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: _width * 0.02, vertical: _height * 0.015),
+              child: Text(
+                second,
+                style: TextStyle(fontSize: _height * 0.021),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: _width * 0.02, vertical: _height * 0.015),
+              child: Text(
+                third,
+                style: TextStyle(fontSize: _height * 0.021),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crazy Slots'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  createSlot(first: '1', second: '2', third: '3'),
+                  createSlot(first: '4', second: '5', third: '6'),
+                  createSlot(first: '7', second: '8', third: '9'),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: _height * 0.1),
+            padding: EdgeInsets.symmetric(
+                horizontal: _width * 0.03, vertical: _height * 0.02),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: Text(
+              'Spin',
+              style: TextStyle(fontSize: _height * 0.021),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
