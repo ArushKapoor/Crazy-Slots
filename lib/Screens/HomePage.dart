b91d659be03f9d3b3bd23239c9f43364fe477eb2
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-12-28 19:24:48 +0530

    added rotate method

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:crazy_slots/Utilities/Algorithm.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
     double _height = MediaQuery.of(context).size.height;
-    Container createSlot({String first, String second, String third}) {
+
+    Algorithm algorithm = new Algorithm();
+    List first = ['1', '2', '3'];
+    List second = ['4', '5', '6'];
+    List third = ['7', '8', '9'];
+
+    List<Object> _rotateList(List<Object> list, int i) {
+      if (list == null || list.isEmpty) return list;
+      return list.sublist(list.length - i)
+        ..addAll(list.sublist(0, list.length - i));
+    }
+
+    void _spin() {
+      setState(() {
+        first = _rotateList(first, 1);
+        print(first);
+      });
+    }
+
+    Container createSlot({List displayList}) {
       return Container(
         decoration: BoxDecoration(
           border: Border.all(color: Colors.black),
@@ -25,7 +45,7 @@ class _HomePageState extends State<HomePage> {
               margin: EdgeInsets.symmetric(
                   horizontal: _width * 0.02, vertical: _height * 0.015),
               child: Text(
-                first,
+                displayList[0],
                 style: TextStyle(fontSize: _height * 0.021),
               ),
             ),
@@ -39,7 +59,7 @@ class _HomePageState extends State<HomePage> {
               margin: EdgeInsets.symmetric(
                   horizontal: _width * 0.02, vertical: _height * 0.015),
               child: Text(
-                second,
+                displayList[1],
                 style: TextStyle(fontSize: _height * 0.021),
               ),
             ),
@@ -47,7 +67,7 @@ class _HomePageState extends State<HomePage> {
               margin: EdgeInsets.symmetric(
                   horizontal: _width * 0.02, vertical: _height * 0.015),
               child: Text(
-                third,
+                displayList[2],
                 style: TextStyle(fontSize: _height * 0.021),
               ),
             ),
@@ -73,23 +93,28 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  createSlot(first: '1', second: '2', third: '3'),
-                  createSlot(first: '4', second: '5', third: '6'),
-                  createSlot(first: '7', second: '8', third: '9'),
+                  createSlot(displayList: first),
+                  createSlot(displayList: second),
+                  createSlot(displayList: third),
                 ],
               ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(top: _height * 0.1),
-            padding: EdgeInsets.symmetric(
-                horizontal: _width * 0.03, vertical: _height * 0.02),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-            ),
-            child: Text(
-              'Spin',
-              style: TextStyle(fontSize: _height * 0.021),
+          GestureDetector(
+            onTap: () {
+              _spin();
+            },
+            child: Container(
+              margin: EdgeInsets.only(top: _height * 0.1),
+              padding: EdgeInsets.symmetric(
+                  horizontal: _width * 0.03, vertical: _height * 0.02),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: Text(
+                'Spin',
+                style: TextStyle(fontSize: _height * 0.021),
+              ),
             ),
           ),
         ],
--- lib/Utilities/Algorithm.dart
+++ lib/Utilities/Algorithm.dart
@@ -3,44 +3,34 @@ import 'dart:math';
 class Algorithm {
   void tempMethod() {
     var rng = new Random();
-    List outcome = new List(3);
-    int count = 0;
-//   for(int y = 0; y < 50; y++) {
-    while (true) {
-      for (int i = 0; i <= 2; i++) {
-        String symbol = '';
-        int num = rng.nextInt(49);
-        if (num < 12) {
-          // 12
-          symbol = 'lemon';
-        } else if (num < 23) {
-          // 11
-          symbol = 'orange';
-        } else if (num < 32) {
-          // 9
-          symbol = 'apple';
-        } else if (num < 39) {
-          // 7
-          symbol = 'pear';
-        } else if (num < 44) {
-          // 5
-          symbol = 'grapes';
-        } else if (num < 47) {
-          // 3
-          symbol = 'bell';
-        } else {
-          // 2
-          symbol = 'cherry';
-        }
-        outcome[i] = symbol;
+    List outcome = new List.filled(3, 0);
+    for (int i = 0; i <= 2; i++) {
+      String symbol = '';
+      int num = rng.nextInt(49);
+      if (num < 12) {
+        // 12
+        symbol = 'lemon';
+      } else if (num < 23) {
+        // 11
+        symbol = 'orange';
+      } else if (num < 32) {
+        // 9
+        symbol = 'apple';
+      } else if (num < 39) {
+        // 7
+        symbol = 'pear';
+      } else if (num < 44) {
+        // 5
+        symbol = 'grapes';
+      } else if (num < 47) {
+        // 3
+        symbol = 'bell';
+      } else {
+        // 2
+        symbol = 'cherry';
       }
-      count++;
-      if (outcome[0] == outcome[1] &&
-          outcome[0] == outcome[2] &&
-          outcome[0] == 'cherry') {
-        break;
-      }
+      outcome[i] = symbol;
     }
-    print(count);
+    print(outcome);
   }
 }
